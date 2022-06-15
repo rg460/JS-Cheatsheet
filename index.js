ccbef0b0852025ee979b3349f474cf074d18b0a5
ravigordon <rgordz460@outlook.com>
2022-06-15 11:15:29 +0100

    arrow function example

--- index.js
+++ index.js
@@ -4,4 +4,32 @@
 
 // Example
 const myName = 'Ravi';
-let myName2 = 'Ravi'
+let myName2 = 'Ravi';
+
+// Arrow Functions
+//  different syntax for writting functions.
+// No more issues with the this keyword.
+// when you use this in a arrow funcion it will always keep its context and not change on Runtime.
+//  Example
+const myFnc =() =>{
+    // code in here
+}
+
+const printMyName = (name) =>{
+    console.log(name);
+}
+printMyName('Ravi')
+
+const printMyName2 = () =>{
+    console.log('Pele')
+}
+printMyName2();
+
+const printMyName3 = (name,age) =>{
+    console.log(name,age);
+}
+printMyName3('Ronaldo', 9)
+
+
+const multiply = (number) => number * 2;
+console.log(multiply(2));
