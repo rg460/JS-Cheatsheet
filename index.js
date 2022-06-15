37e14413b1d39a281a2daea7301f14a4d9f8e0a4
ravigordon <rgordz460@outlook.com>
2022-06-15 11:04:31 +0100

    let and const examples and definitions

---
+++ index.js
@@ -0,0 +1,7 @@
+//  let & const 
+// use let if you want to create a variable that you would like to change.
+// use const if you would like to create a variable that you do not want to change
+
+// Example
+const myName = 'Ravi';
+let myName2 = 'Ravi'
